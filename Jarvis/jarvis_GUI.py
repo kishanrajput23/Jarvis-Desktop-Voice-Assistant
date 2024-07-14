3f2952c610206749e9d26e629145cc53f2d5001c
NathanVials-Student <26464718@students.lincoln.ac.uk>
2024-07-14 12:32:21 +0100

    Add frames to GUI

--- Jarvis/jarvis_GUI.py
+++ Jarvis/jarvis_GUI.py
@@ -10,7 +10,13 @@ app = ctk.CTk()
 app.geometry("500x200")
 app.title("Jarvis")
 
-#
+#create sidebar frame
+sidebar_frame = ctk.CTkFrame(app, width=130, corner_radius=0)
+sidebar_frame.grid(row=0, column=0, sticky='nsew')
+
+#create main frame
+main_frame = ctk.CTkFrame(app, width=300)
+main_frame.grid(row=0, column=1, padx=37)
 
 #run interface
 app.mainloop()
