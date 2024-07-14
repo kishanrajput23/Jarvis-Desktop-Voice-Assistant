bbbe679671666fd3e67749165499a69a3ee06831
NathanVials-Student <26464718@students.lincoln.ac.uk>
2024-07-14 12:47:21 +0100

    Updated GUI
    Finished settings sidebar, currently inactive

--- Jarvis/jarvis_GUI.py
+++ Jarvis/jarvis_GUI.py
@@ -10,13 +10,29 @@ app = ctk.CTk()
 app.geometry("500x200")
 app.title("Jarvis")
 
-#create sidebar frame
-sidebar_frame = ctk.CTkFrame(app, width=130, corner_radius=0)
+#create sidebar frame to hold settings
+sidebar_frame = ctk.CTkFrame(app, width=160, corner_radius=0)
 sidebar_frame.grid(row=0, column=0, sticky='nsew')
 
-#create main frame
-main_frame = ctk.CTkFrame(app, width=300)
+#create main frame to hold jarvis button
+main_frame = ctk.CTkFrame(app, width=270)
 main_frame.grid(row=0, column=1, padx=37)
+
+#add settings label in sidebar
+settings_label = ctk.CTkLabel(sidebar_frame, text="Settings", font= ctk.CTkFont(size=18))
+settings_label.place(x=43, y=10)
+
+#add voice gender dropdown menu with label
+gender_menu_label = ctk.CTkLabel(sidebar_frame, text='Gender')
+gender_menu_label.place(x=55, y=40)
+gender_menu = ctk.CTkOptionMenu(sidebar_frame, values=['Male', 'Female'])
+gender_menu.place(x=10, y=70)
+
+#add light/dark mode dropdown menu with label
+mode_menu_label = ctk.CTkLabel(sidebar_frame, text='Appearence Mode')
+mode_menu_label.place(x=23, y=115)
+mode_menu = ctk.CTkOptionMenu(sidebar_frame, values=['Dark', 'Light'])
+mode_menu.place(x=10, y=150)
 
 #run interface
 app.mainloop()
